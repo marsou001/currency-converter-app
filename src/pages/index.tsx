9e6d8970cf2a4a84f05491de3c6c350583e8b97d
marouane <marsou431@gmail.com>
2024-05-30 14:39:28 +0100

    add benefits section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,9 +49,9 @@ export default function Home() {
         <ConversionOptionChip currencyFrom='USD' currencyTo='CAD' flagIconURL={cad} altText='Canada Flag' />
       </div>
 
-      <h2 className='text-xl font-bold my-16'>Compare Foreign Exchange Rates and Save Money</h2>
+      <h2 className='text-xl font-bold mt-16'>Compare Foreign Exchange Rates and Save Money</h2>
 
-      <div className='bg-white p-8 rounded-lg shadow-gray-200 shadow-lg'>
+      <div className='bg-white my-8 p-8 rounded-lg shadow-gray-200 shadow-lg'>
         <h3 className='font-bold text-center w-3/4 mx-auto mb-8'>Save up to 50% on foreign exchange rates</h3>
       
         <div className="exchange">
@@ -89,6 +89,17 @@ export default function Home() {
             toggleMenu={toggleToMenu}
           />
         </div>
+      </div>
+
+      <div className='benefits text-gray-500'>
+        <h3 className='text-sm'>Get the best exchange rates from leading providers and save money on your international payments</h3>
+        <ul className='text-xs list-disc mt-3 mb-5 pl-3'>
+          <li>Compare exchange rates from leading providers to find the best deal.</li>
+          <li>Save money on your international payments by using a reputable foreign exchange company.</li>
+          <li>Get the most out of your money by comparing foreign exchange rates and choosing the best deal.</li>
+        </ul>
+
+        <button className='text-white bg-blue text-xs py-2 px-5 rounded-md'>Learn More</button>
       </div>
     </>
   )
