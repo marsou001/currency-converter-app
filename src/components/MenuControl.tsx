1964b2f3136ebbb68363e962c3ff37ff7fb64fe0
marouane <marsou431@gmail.com>
2024-09-12 15:51:10 +0100

    cursor-pointer on menu control

--- src/components/MenuControl.tsx
+++ src/components/MenuControl.tsx
@@ -8,7 +8,7 @@ export default function MenuControl(props: MenuControlProps) {
   const arrowDown = '/arrow-down.png';
 
   return (
-    <div className='dropdown-control absolute top-2 right-2 inline-flex' onClick={props.toggleShowMenu}>
+    <div className='dropdown-control absolute top-2 right-2 inline-flex cursor-pointer' onClick={props.toggleShowMenu}>
       <Image src={flagIconURL} width={15} height={15} alt={`${country} flag icon`} className='inline-block mr-1' />
       <span className='text-gray-400'>{ props.currency }</span>
       <Image src={arrowDown} width={25} height={1} alt='Arrow down icon' className='inline-block transform scale-50' />
