be5fa2d9fbe9ddfc1811d462e5342283ccfbc3c5
marouane <marsou431@gmail.com>
2024-10-01 22:44:39 +0100

    make action typing more accurate

--- src/types/Action.ts
+++ src/types/Action.ts
@@ -1,9 +1,24 @@
 import ActionTypes from "@/enums/ActionTypes.enum";
 import State from "./State";
 
-type Action = {
-  type: ActionTypes;
-  payload: Partial<State>;
+type Action = 
+| {
+  type: ActionTypes.SET_AMOUNT;
+  payload: {
+    amount: State["amount"];
+  };
+}
+| {
+  type: ActionTypes.SET_CURRENCY;
+  payload: {
+    currency: State["currency"];
+  };
+}
+| {
+  type: ActionTypes.SET_IS_FETCHING_RATE;
+  payload: {
+    isFetchingRate: State["isFetchingRate"];
+  };
 }
 
 export default Action;
