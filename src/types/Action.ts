ac065a148fe310792bfde3de1e7992d32ac4cc77
marouane <marsou431@gmail.com>
2024-10-08 19:01:05 +0100

    fix Action type indentation

--- src/types/Action.ts
+++ src/types/Action.ts
@@ -3,22 +3,23 @@ import State from "./State";
 
 type Action = 
 | {
-  type: ActionTypes.SET_AMOUNT;
-  payload: {
-    amount: State["amount"];
-  };
-}
+    type: ActionTypes.SET_AMOUNT;
+    payload: {
+      amount: State["amount"];
+    };
+  }
 | {
-  type: ActionTypes.SET_CURRENCY;
-  payload: {
-    currency: State["currency"];
-  };
-}
+    type: ActionTypes.SET_CURRENCY;
+    payload: {
+      currency: State["currency"];
+    };
+  }
 | {
-  type: ActionTypes.SET_IS_FETCHING_RATE;
-  payload: {
-    isFetchingRate: State["isFetchingRate"];
-  };
-}
+    type: ActionTypes.SET_IS_FETCHING_RATE;
+    payload: {
+      isFetchingRate: State["isFetchingRate"];
+    };
+  }
+;
 
 export default Action;
