d1cfd513cd2154b82cfaf8ccd4d7ae0d261ff9f1
marouane <marsou431@gmail.com>
2025-03-24 21:02:30 +0000

    minor edits

--- src/reducerFactory.ts
+++ src/reducerFactory.ts
@@ -5,11 +5,11 @@ function reducerFactory(): StateReducer {
   return function(state: State, action: Action): State {
     switch (action.type) {
       case ActionTypes.SET_AMOUNT:
-        return { ...state, amount: action.payload.amount! };
+        return { ...state, amount: action.payload.amount };
       case ActionTypes.SET_CURRENCY:
-        return { ...state, currency: action.payload.currency! };
+        return { ...state, currency: action.payload.currency };
       case ActionTypes.SET_IS_FETCHING_RATE:
-        return { ...state, isFetchingRate: action.payload.isFetchingRate! };
+        return { ...state, isFetchingRate: action.payload.isFetchingRate };
     }
   }
 }
--- src/types/State.ts
+++ src/types/State.ts
@@ -5,4 +5,5 @@ type State = {
   currency: Currency;
   isFetchingRate: boolean;
 }
+
 export default State;
